4693e806a72887b06f6f4ee19b771f01e0974e5e
velTrinitas <71570164+velTrinitas@users.noreply.github.com>
2021-03-13 22:11:36 +0100

    add set Interval

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  setInterval(getTime, 1000);
+
+  const time = new Date().toLocaleTimeString();
+  const [count, setCount] = useState(time);
+
+  function getTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setCount(newTime);
+    setInterval(getTime, 1000);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
